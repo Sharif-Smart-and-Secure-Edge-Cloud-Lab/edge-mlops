2575e963657115f862c98b4e4337db1c13bb953b
Ehsan <abedinehsan1380@gmail.com>
2023-10-24 19:47:55 +0330

    Added server logs

--- imdb_model/IMDB.py
+++ imdb_model/IMDB.py
@@ -8,7 +8,9 @@ import datetime
 import re
 import onnx
 import tf2onnx
+import logging
 
+from flask_socketio import SocketIO
 from skl2onnx import convert_sklearn
 from skl2onnx.common.data_types import FloatTensorType
 from sklearn.model_selection import train_test_split
@@ -20,6 +22,26 @@ from keras.preprocessing.sequence import pad_sequences
 from keras.models import Sequential
 from keras.layers import Dense, Embedding, LSTM, SpatialDropout1D
 from nltk.corpus import stopwords
+
+# log_file = 'log_output.txt'
+# socketio = SocketIO(message_queue="redis://")
+
+# # Create a custom handler for logging that sends messages to the WebSocket
+# class WebSocketHandler(logging.Handler):
+#     def emit(self, record):
+#         log_message = self.format(record)
+#         socketio.emit('log_message', log_message)
+
+# # Configure the root logger to use the custom handler
+# root_logger = logging.getLogger()
+# root_logger.setLevel(logging.DEBUG)
+# handler = WebSocketHandler()
+# formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+# handler.setFormatter(formatter)
+# root_logger.addHandler(handler)
+# # Also log to console.
+# console = logging.StreamHandler()
+# root_logger.addHandler(console)
 
 def clean_text(text, stop_words):
     text = text.lower() # lowercase text
--- imdb_model/Server.py
+++ imdb_model/Server.py
@@ -2,10 +2,34 @@ import os
 import json
 import time
 import IMDB
+import logging
 import traceback
 from flask import Flask, request, jsonify, render_template
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+socketio = SocketIO(app)
+
+log_file = 'log_output.txt'
+
+# Create a custom handler for logging that sends messages to the WebSocket
+class WebSocketHandler(logging.Handler):
+    def emit(self, record):
+        log_message = self.format(record)
+        socketio.emit('log_message', {'message': log_message}, namespace='/logs')
+        with open(log_file, 'a') as f:
+            f.write(log_message + '\n')
+
+# Configure the root logger to use the custom handler
+root_logger = logging.getLogger()
+root_logger.setLevel(logging.DEBUG)
+handler = WebSocketHandler()
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+root_logger.addHandler(handler)
+# Also log to console.
+console = logging.StreamHandler()
+root_logger.addHandler(console)
 
 @app.route('/')
 def index():
@@ -48,8 +72,23 @@ def run_model():
         return jsonify({'result': accuracy})
     
     except Exception as e:
+        app.logger.error(str(e))
         traceback.print_exc()
         return jsonify({'error': str(e)}), 400
 
+@app.route('/logs')
+def serve_logs():
+    with open(log_file, 'r') as f:
+        log_content = f.read()
+    return render_template('logs.html', log_content=log_content)
+
+@socketio.on('connect', namespace='/logs')
+def connect():
+    app.logger.info('Client connected')
+
+@socketio.on('disconnect', namespace='/logs')
+def disconnect():
+    app.logger.info('Client disconnected')
+
 if __name__ == '__main__':
-    app.run(host='localhost', port=8081)
+    socketio.run(app, host='localhost', port=8081, allow_unsafe_werkzeug=True)
